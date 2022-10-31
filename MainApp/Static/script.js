ce26a10c99b3241eb2c2737de8db41db5f1dfd1e
Buscompany21 <buscobrian1@gmail.com>
2022-10-30 20:23:08 -0600

    worked on table

---
+++ MainApp/Static/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#dtHorizontalVertical').DataTable({
+      "scrollX": true,
+      "scrollY": 200,
+    });
+    $('.dataTables_length').addClass('bs-select');
+  });
